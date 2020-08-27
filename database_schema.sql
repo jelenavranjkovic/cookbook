72117c4aa6cb0aa0abbd0ed441b3f98177b9a62f
unknown <babic.milan999@yahoo.com>
2020-08-27 20:37:25 +0200

    Added creator to post table

--- database_schema.sql
+++ database_schema.sql
@@ -1,19 +1,20 @@
 CREATE TABLE users (
-	id 			bigint primary key auto_increment,
+		id 			bigint primary key auto_increment,
     username 	varchar(50) not null unique,
     password 	varchar(50) not null,
     level 		int not null
 );
 
 CREATE TABLE post (
-	id 			bigint primary key auto_increment,
+		id 			bigint primary key auto_increment,
     type 		varchar(10) not null,
     title 		varchar(50) not null,
-    content 	varchar(1000) not null
+    content 	varchar(1000) not null,
+		creator		bigint not null references users(id)
 );
 
 CREATE TABLE comments (
-	id			bigint primary key auto_increment,
+		id			bigint primary key auto_increment,
     date		timestamp not null,
     content		varchar(500) not null,
     user_id 	bigint not null references users(id),
@@ -21,13 +22,13 @@ CREATE TABLE comments (
 );
 
 CREATE TABLE category (
-	id 			bigint primary key auto_increment,
+		id 			bigint primary key auto_increment,
     title 		varchar(30) not null,
     description varchar(300) not null
 );
 
 CREATE TABLE category_post (
-	post_id 		bigint,
+		post_id 		bigint,
     category_id 	bigint,
     constraint category_post_pk primary key (post_id, category_id)
 );
